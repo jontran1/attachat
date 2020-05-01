0195ec1c73d24277e24f4bab15e45762beb6d18f
Jonathan <jtran001@citymail.cuny.edu>
2020-05-01 17:05:38 -0400

    Create User.java
    Created user class with fields getters and setters and constructors.

---
+++ src/main/java/com/attachat/entity/User.java
@@ -0,0 +1,53 @@
+package com.attachat.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+
+	@Id
+	@Column(name="user_name")
+	private String userName;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="enabled")
+	private boolean enabled;
+
+	public User(String userName, String password, boolean enabled) {
+		this.userName = userName;
+		this.password = password;
+		this.enabled = enabled;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+	
+	
+}
