a32a044bad2526496f5471b3ff0b32ec82d436a3
Jonathan <jtran001@citymail.cuny.edu>
2020-05-01 17:34:08 -0400

    Create SubFollower.java
    hibernate annoatations and getters/setters for SubFollower class.

---
+++ src/main/java/com/attachat/entity/SubFollower.java
@@ -0,0 +1,43 @@
+package com.attachat.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="sub_follower")
+public class SubFollower {
+	
+	@Id
+	@Column(name="user_name")
+	private String userName;
+	
+	@Id
+	@Column(name="sub_name")
+	private String subName;
+
+	public SubFollower(String userName, String subName) {
+		this.userName = userName;
+		this.subName = subName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getSubName() {
+		return subName;
+	}
+
+	public void setSubName(String subName) {
+		this.subName = subName;
+	}
+	
+	
+	
+}
