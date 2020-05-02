65ff6fbf14f5c56e149dfa1f8b2ec882b5a1e180
Jonathan <jtran001@citymail.cuny.edu>
2020-05-02 19:10:36 -0400

    Implemented SubDAO interface.
    Created SubDAO interface and implemented the interface with SubDAOImpl class.

---
+++ src/main/java/com/jon/attachat/dao/SubDAO.java
@@ -0,0 +1,15 @@
+package com.jon.attachat.dao;
+
+import com.jon.attachat.entity.Sub;
+import java.util.List;
+
+public interface SubDAO {
+	
+	public List<Sub> getSubs();
+	
+	public Sub getSub(String subName);
+	
+	public void saveSub(Sub sub);
+	
+	public void deleteSub(Sub sub);
+}
---
+++ src/main/java/com/jon/attachat/dao/SubDAOImpl.java
@@ -0,0 +1,59 @@
+package com.jon.attachat.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.hibernate.query.Query;
+
+import com.jon.attachat.entity.Sub;
+
+@Repository
+public class SubDAOImpl implements SubDAO {
+	
+	// need to inject the session factory
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	public List<Sub> getSubs() {
+
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		Query<Sub> query =
+				currentSession.createQuery("from sub", Sub.class);
+		
+		List<Sub> subs = query.getResultList();
+		
+		return subs;
+	}
+
+	@Override
+	public Sub getSub(String subName){
+		// Get current session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		Sub sub = currentSession.get(Sub.class, subName);
+		
+		return sub;
+	}
+
+	@Override
+	public void saveSub(Sub sub) {
+		// Get current hibernate session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		currentSession.saveOrUpdate(sub);
+	}
+
+	@Override
+	public void deleteSub(Sub sub) {
+		// Get current session
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		currentSession.delete(sub);
+	}
+
+}
