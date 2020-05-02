296b79ce4689a6faaef59c6d1aee2a48c93f9535
Jonathan <jtran001@citymail.cuny.edu>
2020-05-02 18:13:12 -0400

    Create AuthoritieDAO.java
    Basic interface for basic CRUD operations for Authoritie.

---
+++ src/main/java/com/jon/attachat/dao/AuthoritieDAO.java
@@ -0,0 +1,11 @@
+package com.jon.attachat.dao;
+
+import com.jon.attachat.entity.Authoritie;
+
+public interface AuthoritieDAO {
+	
+	public void saveAuthoritie(Authoritie authoritie);
+	
+	public void deleteAuthoritie(Authoritie authoritie);
+
+}
