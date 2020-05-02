a898f709905ca7ad33a48285334948ddf460e53f
Jonathan <jtran001@citymail.cuny.edu>
2020-05-02 15:41:06 -0400

    Update UserDAO.java
    Add itnerface methods for saveUser, getUser, deleteUser for basic CRUD operations.

--- src/main/java/com/jon/attachat/dao/UserDAO.java
+++ src/main/java/com/jon/attachat/dao/UserDAO.java
@@ -8,5 +8,11 @@ import com.jon.attachat.entity.User;
 public interface UserDAO {
 	
 	public List<User> getUsers();
+	
+	public void saveUser(User user);
+	
+	public User getUser(String userName);
+	
+	public void deleteUser(User user);
 
 }
