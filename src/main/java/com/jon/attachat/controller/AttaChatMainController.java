06e723fc92d9d20067de3b776beda858c6e1e491
Jonathan <jtran001@citymail.cuny.edu>
2020-05-02 19:47:31 -0400

    Display subs and creators on home.jsp.
    Manage to display subs and creators on home.jsp.

--- src/main/java/com/jon/attachat/controller/AttaChatMainController.java
+++ src/main/java/com/jon/attachat/controller/AttaChatMainController.java
@@ -1,13 +1,35 @@
 package com.jon.attachat.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.jon.attachat.dao.AuthoritieDAO;
+import com.jon.attachat.dao.UserDAO;
+import com.jon.attachat.entity.Authoritie;
+import com.jon.attachat.entity.Sub;
+import com.jon.attachat.entity.User;
+import com.jon.attachat.service.SubService;
+import com.jon.attachat.service.UserService;
 
 @Controller
 public class AttaChatMainController {
 	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private SubService subService;
+		
 	@GetMapping("/")
-	public String showHome() {
+	public String showHome(Model model) {
+		List<Sub> subs = subService.getSubs();
+		
+		model.addAttribute("subs", subs);
+		
 		return "home";
 	}
 	
