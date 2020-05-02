3269d36784a86a139d91e0078f9b8333fd58ac61
Jonathan <jtran001@citymail.cuny.edu>
2020-05-02 18:49:22 -0400

    Added @Service annotation.

--- src/main/java/com/jon/attachat/service/UserServiceImpl.java
+++ src/main/java/com/jon/attachat/service/UserServiceImpl.java
@@ -2,7 +2,9 @@ package com.jon.attachat.service;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.jon.attachat.dao.AuthoritieDAO;
@@ -10,6 +12,7 @@ import com.jon.attachat.dao.UserDAO;
 import com.jon.attachat.entity.Authoritie;
 import com.jon.attachat.entity.User;
 
+@Service
 public class UserServiceImpl implements UserService {
 	
 	@Autowired
