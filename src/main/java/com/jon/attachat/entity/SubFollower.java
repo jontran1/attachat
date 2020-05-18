dbacba33c2639571318e91c49e90e2163a13f84f
Jonathan Tran <43768831+jontran1@users.noreply.github.com>
2020-05-18 13:08:16 -0400

    Created SubFollower entity.

---
+++ src/main/java/com/jon/attachat/entity/SubFollower.java
@@ -0,0 +1,52 @@
+package com.jon.attachat.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@IdClass(SubFollowerId.class)
+@Table(name="sub_follower")
+public class SubFollower {
+	
+	@Id
+	@JoinColumn(name="user_name")
+	@ManyToOne
+	private User user;
+	
+	@Id
+	@JoinColumn(name="sub_name")
+	@ManyToOne
+	private Sub sub;
+
+	public SubFollower(User user, Sub sub) {
+		this.user = user;
+		this.sub = sub;
+	}
+
+	public SubFollower() {
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Sub getSub() {
+		return sub;
+	}
+
+	public void setSub(Sub sub) {
+		this.sub = sub;
+	}
+
+
+	
+}
---
+++ src/main/java/com/jon/attachat/entity/SubFollowerId.java
@@ -0,0 +1,11 @@
+package com.jon.attachat.entity;
+
+import java.io.Serializable;
+
+public class SubFollowerId implements Serializable {
+	
+	private User user;
+	
+	private Sub sub;
+
+}
