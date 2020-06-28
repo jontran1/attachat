590757b659bd67f544ea14fb53b34b93ff4ddee9
Jonathan Tran <43768831+jontran1@users.noreply.github.com>
2020-06-28 00:22:35 -0400

    Added comments to SecurityWebApplicationInitializer

--- src/main/java/com/jon/attachat/config/SecurityWebApplicationInitializer.java
+++ src/main/java/com/jon/attachat/config/SecurityWebApplicationInitializer.java
@@ -2,6 +2,10 @@ package com.jon.attachat.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+/*
+ * This file is needed for spring security. You must create a class that extends
+ * AbstractSecurityWebApplicationInitializer.
+ */
 public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
 
 }
