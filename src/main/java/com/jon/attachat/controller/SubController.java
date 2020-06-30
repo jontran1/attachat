c8cb272119daab1b354d3a516f7515b231b2500f
Jonathan Tran <43768831+jontran1@users.noreply.github.com>
2020-06-30 00:20:41 -0400

    the initBinder function will trim all string input of spaces.

--- src/main/java/com/jon/attachat/controller/SubController.java
+++ src/main/java/com/jon/attachat/controller/SubController.java
@@ -3,11 +3,16 @@ package com.jon.attachat.controller;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +40,18 @@ public class SubController {
 	
 	@Autowired
 	private ThreadService threadService;
+	
+	/*
+	 * All string inputs will being routed into this controller
+	 * have all all white spaces trimmed.
+	 */
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
 	
 	@GetMapping("/showSub")
 	public String showSub(@RequestParam("subName") String subName, Model model,
