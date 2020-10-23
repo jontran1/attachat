4a0c186ff02c9295e6d94c327491fd9e369ee85c
Jonathan Tran <43768831+jontran1@users.noreply.github.com>
2020-10-23 15:16:07 -0400

    Remove unused imports.

--- src/main/java/com/jon/attachat/controller/AttaChatMainController.java
+++ src/main/java/com/jon/attachat/controller/AttaChatMainController.java
@@ -12,16 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.jon.attachat.dao.AuthoritieDAO;
-import com.jon.attachat.dao.UserDAO;
-import com.jon.attachat.entity.Authoritie;
 import com.jon.attachat.entity.Sub;
-import com.jon.attachat.entity.SubFollower;
-import com.jon.attachat.entity.User;
 import com.jon.attachat.service.SubService;
 import com.jon.attachat.service.ThreadService;
 import com.jon.attachat.service.UserService;
-import com.jon.attachat.entity.Thread;
 
 @Controller
 public class AttaChatMainController {
